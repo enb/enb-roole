b69ad07f8a8f64643df8f4c364a87fb412620a83
Alexej Yaroshevich <alex@qfox.ru>
2014-08-27 17:40:44 +0400

    allow @imports in roole files Roole support @import itself but before squashing block style files it should be resolved But easier to import them by csspreprocessor provided by enb package closes #6

--- test/func/test.css-roole.js
+++ test/func/test.css-roole.js
@@ -2,6 +2,7 @@ var TestTargets = require('../lib/test-targets').TestTargets;
 var targets = new TestTargets([
     'bundles/css-only/css-only.css',
     'bundles/roo-only/roo-only.css',
+    'bundles/roo-with-import/roo-with-import.css',
     'bundles/mix/mix.css'
 ]);
 var css;
@@ -21,6 +22,13 @@ describe('css-roole', function () {
 
     it('must build roole', function () {
         css['bundles/roo-only/roo-only.css'].must.include('.roo-block');
+    });
+
+    it('must build roole with @import', function () {
+        var target = css['bundles/roo-with-import/roo-with-import.css'];
+        target.must.include('.roo-block-with-import__elem');
+        target.must.include('.roo-block-with-import .sub');
+        target.must.include('.roo-block-with-import__elem .sub');
     });
 
     it('must build css & roole', function () {
